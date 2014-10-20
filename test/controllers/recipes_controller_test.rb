23f4f4579958ab0e9f45d03e6cf774c03c1b1782
aCandidMind <opensource@candidminds.com>
2014-10-20 07:49:34 +0200

    RecipesControllerTest wasn't updated, when controller ivar was changed

--- test/controllers/recipes_controller_test.rb
+++ test/controllers/recipes_controller_test.rb
@@ -8,7 +8,7 @@ class RecipesControllerTest < ActionController::TestCase
   test "should get index" do
     get :index
     assert_response :success
-    assert_not_nil assigns(:recipes)
+    assert_not_nil assigns(:tags)
   end
 
   test "should get new" do
