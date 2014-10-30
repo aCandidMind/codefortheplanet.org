262010ef073c5c60d6fde4c8eb57d1c46acb3fb2
aCandidMind <opensource@candidminds.com>
2014-10-31 00:12:32 +0100

    Setting secret_key_base in production.rb to avoid committing secrets.yml

--- config/environments/production.rb
+++ config/environments/production.rb
@@ -1,6 +1,9 @@
 Rails.application.configure do
   # Settings specified here will take precedence over those in config/application.rb.
 
+  # setting secret_key_base here to not be forced to commit secrets.yml
+  config.secret_key_base = ENV["SECRET_KEY_BASE"]
+
   # Code is not reloaded between requests.
   config.cache_classes = true
 
