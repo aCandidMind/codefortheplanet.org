77bb30bb5422e8e14dc96e223fe6529aab0144a4
aCandidMind <opensource@candidminds.com>
2014-11-07 02:49:40 +0100

    Added more helpful error messages to ContactTest should tests fail

--- test/models/contact_test.rb
+++ test/models/contact_test.rb
@@ -8,13 +8,15 @@ class ContactTest < ActiveSupport::TestCase
 
   test "should not save without any options" do
     contact = Contact.new
-    refute contact.save, "No contact option was provided"
+    was_saved = contact.save
+    assert_equal [:base], contact.errors.keys, "should only have an error on :base"
+    refute was_saved, "No contact option was provided"
   end
 
   test "should save with one option set" do
     Contact.valid_options.each do |option|
       contact = Contact.new(option => "test")
-      assert contact.save
+      assert contact.save, "expected to save, but didn't: #{contact.errors.full_messages}"
     end
   end
 
