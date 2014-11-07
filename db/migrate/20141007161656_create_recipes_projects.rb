8cfc32aee6f48b83cafcc13879c536d56be84128
aCandidMind <opensource@candidminds.com>
2014-11-07 02:37:10 +0100

    Changing order of column creation to be more consistent with table name

--- db/migrate/20141007161656_create_recipes_projects.rb
+++ db/migrate/20141007161656_create_recipes_projects.rb
@@ -1,8 +1,8 @@
 class CreateRecipesProjects < ActiveRecord::Migration
   def change
     create_table :recipes_projects, id: false do |t|
-      t.integer :project_id, null: false
       t.integer :recipe_id, null: false
+      t.integer :project_id, null: false
     end
   end
 end
