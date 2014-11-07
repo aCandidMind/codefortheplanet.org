1e208e65feb29d7638f53182ac6e7ceabb8a8bd8
aCandidMind <opensource@candidminds.com>
2014-11-07 02:30:54 +0100

    Setting page specific <title> in <head>

--- app/helpers/application_helper.rb
+++ app/helpers/application_helper.rb
@@ -1,2 +1,10 @@
 module ApplicationHelper
+
+  def localized_model_string(key, model, options = {})
+    options = options.dup
+    model_count = options.delete(:count) || 1
+    options.merge!(class_name: model.model_name.human(count: model_count))
+    I18n.t(key, options)
+  end
+
 end
