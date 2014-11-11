e56b63679da4c8b22e5e4a56ef8ed9d75fde80f1
aCandidMind <opensource@candidminds.com>
2014-11-11 08:34:58 +0100

    Concise contact information & improved styling of it in recipe listing

--- app/helpers/recipes_helper.rb
+++ app/helpers/recipes_helper.rb
@@ -6,4 +6,29 @@ module RecipesHelper
     tag_association.map(&:name).join(',')
   end
 
+  def contact_options(contact)
+    set = (Contact.valid_options - ["name"]).
+          select {|option| contact.send(option).present?}.map(&:to_sym)
+    icon_names = {
+      website: "fi-general-website",
+      email: "fi-general-mail",
+      telephone: "fi-general-phone",
+      twitter_handle: "fi-social-twitter",
+      facebook_name: "fi-social-facebook",
+    }
+    base_url = {
+      website: "",
+      email: "mailto:",
+      telephone: "tel:",
+      twitter_handle: "https://twitter.com/",
+      facebook_name: "https://www.facebook.com/search/results/?init=quick&q=",
+    }
+    set.map do |option|
+      icon_name = icon_names[option]
+      link_text = contact.send(option)
+      link_target = base_url[option] + link_text
+      [option, icon_name, link_text, link_target]
+    end
+  end
+
 end
--- app/models/contact.rb
+++ app/models/contact.rb
@@ -8,6 +8,14 @@ class Contact < ActiveRecord::Base
     %w{name website email telephone twitter_handle facebook_name }
   end
 
+  # Assuming country_select is used with Contact attribute `country`
+  # This will attempt to translate the country name and use the default
+  # (usually English) name if no translation is available
+  def country_name
+    country = ISO3166::Country[self.country]
+    country.translations[I18n.locale.to_s] || country.name
+  end
+
   protected
 
     def has_at_least_one_option
