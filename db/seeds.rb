51e81e3cefd06309bc45e73b7541ff85b170e889
aCandidMind <opensource@candidminds.com>
2014-11-14 07:37:09 +0100

    Added a recipe to the seeds & made them create only if name doesn't exist

--- db/seeds.rb
+++ db/seeds.rb
@@ -1,8 +1,9 @@
 # This file should contain all the record creation needed to seed the database with its default values.
 # The data can then be loaded with the rake db:seed (or created alongside the db with db:setup).
 
+name = "A Scalable and Efficient Protocol for Anonymous Communication"
 Recipe.create(
-  name: "A Scalable and Efficient Protocol for Anonymous Communication",
+  name: name,
   description: '<em>The following is a description taken from <a href="https://www.findpaperswelove.com">findpaperswelove.com</a></em>.
   Anonymity is increasingly important for networked applications amidst concerns over censorship and privacy.
   In this paper, we describe Herbivore, a peer-to-peer, scalable, tamper-resilient communication system that
@@ -14,10 +15,11 @@ Recipe.create(
   achieve high bandwidths and low latencies when deployed over the Internet.',
   tech: ["Distributed Systems", "Scalability", "Cryptography"],
   purposes: ["Privacy", "Civic Tech"],
-)
+) unless Recipe.exists?(name: name)
 
+name = "Android port of an iOS app for children that enables them to acoustically experience foreign languages"
 Recipe.create(
-  name: "Android port of an iOS app for children that makes them acoustically experience foreign languages",
+  name: name,
   description: "There's an existing iOS app which enables children to acoustically experience foreign languages
   supporting the understanding among nations and get a sensibility for their multi-cultural peers.
 
@@ -25,10 +27,23 @@ Recipe.create(
   you're interested.",
   tech: ["Sound", "App Development"],
   purposes: ["Child Development", "Integration", "Understanding Among Nations"]
-)
+) unless Recipe.exists?(name: name)
 
+name = "Spotter & Finder (Geolocation) app for toilets for the disabled"
 Recipe.create(
-  name: "Assessing the cleanliness of local restaurants with open data",
+  name: name,
+  description: "The app itself will enable users to enter toilets for the disabled into a database. This database
+  and its Web API have to be developed as well. A user wanting to enter a toilet is supported with a 'Find Me' button.
+  In the end its at least preferrable that the user enters the actual address and further details on how to find the
+  toilet. For initial data there are some offline databases that can be imported into the database that was developed
+  for the app.",
+  tech: ["Geolocation", "Open Data", "App Development", "APIs"],
+  purposes: ["Coping With Everyday Life", "Civic Tech"]
+) unless Recipe.exists?(name: name)
+
+name = "Assessing the cleanliness of local restaurants with open data"
+Recipe.create(
+  name: name,
   description: 'Develop a geolocation app - be it mobile or for the web - which makes it very easy and fast
   (because if it takes too many steps it won\'t be used) to get public health reports for restaurants near the user.
 
@@ -40,7 +55,7 @@ Recipe.create(
   recipe and add them below.',
   tech: ["Open Data", "Geolocation", "App Development", "Web Development"],
   purposes: ["Stopping Diseases", "Enlightenment & Awareness"]
-)
+) unless Recipe.exists?(name: name)
 
 # Recipe.create(
 #   name: "",
