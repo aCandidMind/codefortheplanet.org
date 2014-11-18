3806d033ea68287416e4dcd35f9bbe70e8aa8cac
aCandidMind <opensource@candidminds.com>
2014-11-18 08:12:43 +0100

    Recipe form ignores nested contact_attributes if they're all blank

--- app/models/recipe.rb
+++ app/models/recipe.rb
@@ -6,7 +6,7 @@ class Recipe < ActiveRecord::Base
   has_and_belongs_to_many :projects, join_table: :recipes_projects
   has_and_belongs_to_many :contacts, join_table: :recipes_contacts
 
-  accepts_nested_attributes_for :contacts
+  accepts_nested_attributes_for :contacts, reject_if: :all_blank
 
   validates_presence_of :name, :description
 
